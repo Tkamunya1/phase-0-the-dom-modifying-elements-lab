63902fbfdfb09470a5c8a4aee2d6de277464661d
Titus <titus.kamunya@student.moringaschool.com>
2022-06-07 10:06:22 +0300

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // Write your code here!
+const body = document.querySelector('body')
+body.removeChild(document.querySelector('#main'));
+const newHeader = document.createElement('h1');
+newHeader.id  = 'victory';
+newHeader.textContent = 'T Kamunya is the champion';
+body.appendChild(newHeader);
